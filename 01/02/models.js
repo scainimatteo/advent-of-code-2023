7f368d228ada4dd267cadb9527353b7f3e3d5d94
scainimatteo <matteo.scaini.02031999@gmail.com>
2023-12-02 10:41:03 +0100

    Moved models in separated file

---
+++ 01/02/models.js
@@ -0,0 +1,58 @@
+export const stringToNumber = {
+  'one': 1,
+	'two': 2,
+	'three': 3,
+	'four': 4,
+	'five': 5,
+	'six': 6,
+	'seven': 7,
+	'eight': 8,
+	'nine': 9,
+};
+
+export const stringNumberMap = {
+  0: [
+    'o',
+    't',
+    'f',
+    's',
+    'e',
+    'n',
+  ],
+  1: [
+    'on',
+    'tw',
+    'th',
+    'fo',
+    'fi',
+    'si',
+    'se',
+    'ei',
+    'ni',
+  ],
+  2: [
+    'one',
+    'two',
+    'thr',
+    'fou',
+    'fiv',
+    'six',
+    'sev',
+    'eig',
+    'nin',
+  ],
+  3: [
+    'thre',
+    'four',
+    'five',
+    'seve',
+    'eigh',
+    'nine',
+
+  ],
+  4: [
+    'three',
+    'seven',
+    'eight',
+  ]
+}
